71d123ce1aa16771c916fdbaeea20938265ce218
CQD <cqd@hiigara.net>
2020-08-06 19:32:02 +0800

    把主說明放到 README.md

--- src/Controller/IndexController.php
+++ src/Controller/IndexController.php
@@ -7,27 +7,7 @@ class IndexController extends ControllerBase
     public function run($params)
     {
         header('Content-Type: text/plain');
-        echo <<<EOT
-# CQD 的 Feed 產生器
-
-有些東西沒有 RSS Feed，但是我想訂閱，所以自己寫。
-
-
-## Github
-
-- /github/repo/{user}/{repo}/issuecomment
-  - 某個 Repo 的所有 Issue/PR 的回應
-  - 例如：https://feed.cqd.tw/github/repo/composer/composer/issuecomment
-
-## Vocus
-
-- /vocus/user/{id}
-  - 某個專題的最新文章
-  - 例如：https://feed.cqd.tw/vocus/user/sophist4ever
-- /vocus/publication/{id}
-  - 某個專題的最新文章
-  - 例如：https://feed.cqd.tw/vocus/publication/sophist4ever
-EOT;
+        echo file_get_contents(__DIR__ . '/../../README.md');
     }
 
     public function logic(array $params): array
