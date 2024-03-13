0e001c0dd0a28ede86084f2b6567bf56f701ee1a
CQD <cqd@hiigara.net>
2024-03-14 00:16:58 +0800

    網址參數應該被 urldecoee

--- public/index.php
+++ public/index.php
@@ -6,6 +6,10 @@ date_default_timezone_set('Asia/Taipei');
 
 list($controller, $params) = route($argv[1] ?? $_SERVER['REQUEST_URI']);
 
+foreach ($params as $k => $v) {
+    $params[$k] = urldecode($v);
+}
+
 if (!$controller) {
     http_response_code(404);
 } else {
